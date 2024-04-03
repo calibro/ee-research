af88b711dd694e6539f786e709b770a0e8c49c8a
plcdnl <plcdnl@users.noreply.github.com>
2024-04-03 20:10:48 +0000

    deploy: d343c3e95f83339526db82f3b65b67359a76d523

---
+++ _app/immutable/chunks/stores.Ds3Qihga.js
@@ -0,0 +1,1 @@
+import{s as r}from"./entry.Bsd8_cOn.js";const e=()=>{const s=r;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},a={subscribe(s){return e().page.subscribe(s)}},i={subscribe(s){return e().navigating.subscribe(s)}};export{i as n,a as p};
