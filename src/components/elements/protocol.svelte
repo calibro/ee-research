5694ba70c3667dad5360906f62ebd419be8d1b2a
uf0 <scnepa222@gmail.com>
2024-04-16 10:37:02 +0200

    minor on protocol style

--- src/components/elements/protocol.svelte
+++ src/components/elements/protocol.svelte
@@ -1,70 +1,56 @@
 <script>
-	export let open = false
-	import { slide } from "svelte/transition"
-	import { getProtocol } from "~/lib/metadata"
-	import Text from "./text.svelte"
+  export let open = false;
+  import { slide } from "svelte/transition";
+  import { getProtocol } from "~/lib/metadata";
+  import Text from "./text.svelte";
 
-	export let topic
-	const protocol = getProtocol(topic)
+  export let topic;
+  const protocol = getProtocol(topic);
 
-	let el
+  let el;
 
-	const handleClick = () => {
-		open = !open
-	}
+  const handleClick = () => {
+    open = !open;
+  };
 </script>
 
 <div class="accordion p-xs" bind:this={el}>
-	<button class="header text-left" on:click={handleClick}>
-		<div class="text">
-			<Text content="Protocol" />
-		</div>
-		<Text content={open ? "–" : "+"} />
-	</button>
-	{#if open}
-		<ol class="details" transition:slide>
-			{#each protocol as item}
-				<li class="typo-small">
-					<Text content={item} typo="small" />
-				</li>
-			{/each}
-		</ol>
-	{/if}
+  <button class="header text-left" on:click={handleClick}>
+    <div class="text">
+      <Text content="Protocol" />
+    </div>
+    <Text content={open ? "–" : "+"} />
+  </button>
+  {#if open}
+    <ol class="details" transition:slide>
+      {#each protocol as item}
+        <li>
+          <Text content={item} />
+        </li>
+      {/each}
+    </ol>
+  {/if}
 </div>
 
 <style>
-	.accordion {
-		margin: 1rem 0;
-		border: var(--border-default);
-		border-radius: var(--border-radius);
-	}
+  .accordion {
+    margin: 1rem 0;
+    border: var(--border-default);
+    border-radius: var(--border-radius);
+  }
 
-	ol {
-		counter-reset: item;
-		padding: 0;
-		margin: 0;
-		padding-top: var(--space-xs);
-	}
-	li {
-		display: inline-flex;
-		list-style-type: none;
-		counter-increment: item;
-	}
+  ol {
+    padding-inline-start: 1.5em;
+  }
+  li {
+    margin-bottom: 0.5em;
+  }
 
-	li:before {
-		display: inline;
-		width: 1.5em;
-		padding-right: 0.5em;
-		font-weight: bold;
-		text-align: right;
-		content: counter(item) ".";
-	}
-
-	.header {
-		display: flex;
-		width: 100%;
-		.text {
-			flex: 1;
-		}
-	}
+  .header {
+    display: flex;
+    width: 100%;
+    .text {
+      flex: 1;
+    }
+  }
 </style>
